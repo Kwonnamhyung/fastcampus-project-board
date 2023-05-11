0b24ab0da786069f5ae1fe9e646d7589797ca4a6
Kwonnamhyung <rnjsskagud@gmail.com>
2023-05-11 18:42:16 +0900

    #10 - 도메인 클래스 코드 변환
    도메인을 클래스로 변환

--- fastcampus-project-board/src/main/java/com/fastcampus/projectboard/domain/Article.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -1,11 +1,58 @@
 package com.fastcampus.projectboard.domain;
 
-public class Article {
+import com.fastcampus.projectboard.common.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Article extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Setter @Column(nullable = false)
     private String title;
+    @Setter @Column(nullable = false , length = 10000)
     private String content;
+    @Setter
     private String hashtag;
 
+    @OneToMany(mappedBy = "article" , cascade = CascadeType.ALL , fetch = FetchType.LAZY) @OrderBy("id") @ToString.Exclude // tostring 연관관계 참조 끊는것
+    private final Set<ArticleComment> articleComment = new LinkedHashSet<>();
 
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    public static Article of(String title , String content , String hashtag) {
+        return new Article(title , content , hashtag);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Article article = (Article) o;
+        return getId() != null && getId().equals(article.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
 }
--- fastcampus-project-board/src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
@@ -1,9 +1,49 @@
 package com.fastcampus.projectboard.domain;
 
-public class ArticleComment {
+import com.fastcampus.projectboard.common.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.util.Objects;
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "content"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ArticleComment extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Setter @Column(nullable = false)
     private String content;
+    @Setter @ManyToOne(optional = false , fetch = FetchType.LAZY)
     private Article article;
 
+    private ArticleComment(String content, Article article) {
+        this.content = content;
+        this.article = article;
+    }
+
+    public static ArticleComment of(Article article , String content) {
+        return new ArticleComment(content , article);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ArticleComment that = (ArticleComment) o;
+        return getId() != null && getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
 }
