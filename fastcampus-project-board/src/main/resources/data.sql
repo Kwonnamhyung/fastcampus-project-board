f95e60105941f55e16fda1c2af6f5986720cd517
Kwonnamhyung <rnjsskagud@gmail.com>
2023-05-13 14:20:47 +0900

    #5 - data rest 설정

--- fastcampus-project-board/src/main/resources/data.sql
+++ fastcampus-project-board/src/main/resources/data.sql
@@ -3,3 +3,6 @@ values ('test title' , 'test content' , 'test hashtag' , LOCALTIMESTAMP(0) , 'KN
 
 insert into article_comment(article_id , content , created_at , created_by , modified_at , modified_by)
 values ((select id from article where article.title = 'test title') , 'test content', LOCALTIMESTAMP(0) , 'KNH' , LOCALTIMESTAMP(0) , 'KNH');
+
+insert into article_comment(article_id , content , created_at , created_by , modified_at , modified_by)
+values ((select id from article where article.title = 'test title') , 'test content22', LOCALTIMESTAMP(0) , 'KNH' , LOCALTIMESTAMP(0) , 'KNH');
