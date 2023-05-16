72bfb20dfd4ddedc02b9d206f2f929bd6aa5d7e2
Kwonnamhyung <rnjsskagud@gmail.com>
2023-05-16 22:23:29 +0900

    #16 - 게시글 관련 뷰 테스트 작성

---
+++ fastcampus-project-board/src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -0,0 +1,81 @@
+package com.fastcampus.projectboard.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("View 컨트롤러 - 게시글")
+@WebMvcTest(ArticleController.class)
+class ArticleControllerTest {
+
+    private final MockMvc mvc;
+
+    public ArticleControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+
+    @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지")
+    @Test
+    public void givenNoting_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
+
+        //Given
+
+        //When
+        mvc.perform(get("/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles"));
+        //Then
+
+    }
+
+    @DisplayName("[view][GET] 게시글 상세 페이지")
+    @Test
+    public void givenNoting_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
+
+        //Given
+
+        //When
+        mvc.perform(get("/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("article"));
+        //Then
+
+    }
+
+    @DisplayName("[view][GET] 게시글 검색 전용 페이지")
+    @Test
+    public void givenNoting_whenRequestingArticleSearchView_thenReturnsArticleSearchView() throws Exception {
+
+        //Given
+
+        //When
+        mvc.perform(get("/articles/search"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+        //Then
+
+    }
+
+    @DisplayName("[view][GET] 게시글 해시태그 검색 페이지")
+    @Test
+    public void givenNoting_whenRequestingArticleHashtagSearchView_thenReturnsArticleHashtagSearchView() throws Exception {
+
+        //Given
+
+        //When
+        mvc.perform(get("/articles/search-hashtag"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+        //Then
+
+    }
+}
