c1def14328eaf1436d40e1800e93ed5a7082d0f9
Kwonnamhyung <rnjsskagud@gmail.com>
2023-05-31 22:04:00 +0900

    #19 - 서비스 로직 테스트 작성

---
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/dto/ArticleCommentDto.java
@@ -0,0 +1,22 @@
+package com.fastcampus.projectboard.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * A DTO for the {@link com.fastcampus.projectboard.domain.ArticleComment} entity
+ */
+@Data
+public class ArticleCommentDto {
+    private final LocalDateTime createdAt;
+    private final String createdBy;
+    private final LocalDateTime modifiedAt;
+    private final String modifiedBy;
+    private final String content;
+
+
+    public static ArticleCommentDto of(LocalDateTime createdAt , String createdBy , LocalDateTime modifiedAt , String modifiedBy , String content) {
+        return new ArticleCommentDto(createdAt, createdBy, modifiedAt, modifiedBy, content);
+    }
+}
